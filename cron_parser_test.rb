a90b6d34cf6ee40e705d47b65a6ffbd1e3503b35
Kartik Agarwal <32024148+kartikag1@users.noreply.github.com>
2024-02-15 18:31:13 +0530

    Update cron_parser_test.rb

--- cron_parser_test.rb
+++ cron_parser_test.rb
@@ -7,11 +7,11 @@ RSpec.describe CronParser do
       let(:cron_string) { "*/15 0 1,15 * 1-5 /usr/bin/find" }
       let(:expected_output) do
         {
-          "minute" => "00 15 30 45",
-          "hour" => "00",
-          "day of month" => "01 15",
-          "month" => "01 02 03 04 05 06 07 08 09 10 11 12",
-          "day of week" => "01 02 03 04 05",
+          "minute" => "0 15 30 45",
+          "hour" => "0",
+          "day of month" => "1 15",
+          "month" => "1 2 3 4 5 6 7 8 9 10 11 12",
+          "day of week" => "1 2 3 4 5",
           "command" => "/usr/bin/find"
         }
       end
@@ -27,11 +27,11 @@ RSpec.describe CronParser do
       let(:cron_string) { "*/2 */3 1-5/2 */4 */5 /usr/bin/find" }
       let(:expected_output) do
         {
-          "minute" => "00 02 04 06 08 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54 56 58",
-          "hour" => "00 03 06 09 12 15 18 21",
-          "day of month" => "01 03 05",
-          "month" => "01 05 09",
-          "day of week" => "00 05",
+          "minute" => "0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54 56 58",
+          "hour" => "0 3 6 9 12 15 18 21",
+          "day of month" => "1 3 5",
+          "month" => "1 5 9",
+          "day of week" => "0 5",
           "command" => "/usr/bin/find"
         }
       end
@@ -47,11 +47,11 @@ RSpec.describe CronParser do
       let(:cron_string) { "*/10 3-5 */2 1-3 */4 /usr/bin/find" }
       let(:expected_output) do
         {
-          "minute" => "00 10 20 30 40 50",
-          "hour" => "03 04 05",
-          "day of month" => "01 03 05 07 09 11 13 15 17 19 21 23 25 27 29 31",
-          "month" => "01 02 03",
-          "day of week" => "00 04",
+          "minute" => "0 10 20 30 40 50",
+          "hour" => "3 4 5",
+          "day of month" => "1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31",
+          "month" => "1 2 3",
+          "day of week" => "0 4",
           "command" => "/usr/bin/find"
         }
       end
